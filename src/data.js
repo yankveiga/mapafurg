7f6a928771502edbb58511d27ff2a455ef5973d9
Yan Veiga <yankveiga@gmail.com>
2026-05-28 17:17:14 -0300

    biblioteca

--- src/data.js
+++ src/data.js
@@ -101,8 +101,8 @@ export const predios = [
       "Segunda": "08h às 12h",
       "Terça": "13h às 17h",
       "Quarta": "17h às 21h",
-      "Quinta": "14h às 18h",
-      "Sexta": "14h às 18h"
+      "Quinta": "13h às 17h",
+      "Sexta": "08h às 12h"
     },
     descricao: "⚠️ Operação em horário reduzido devido à greve.\nEspaço para pesquisa e empréstimo de livros.\n📸 @sibfurg \n✉️ sib.direcao@furg.br \n📞 (53) 3293-5388",
   },
